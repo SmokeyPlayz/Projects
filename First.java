ed12dc6b7b81b8d8e8bfda3a189ef3c7b6935b2a
Damarcus Crowell <smokeyplayzyt@gmail.com>
2026-08-23 22:44:58 -0400

    Add First.java - career program

---
+++ First.java
@@ -0,0 +1,12 @@
+// Damarcus Crowell
+// First.java
+// CS Assignment: Java program + career research (Occupational Outlook Handbook)
+
+public class First {
+    public static void main(String[] args) {
+        System.out.println("Hola mundo!");
+        System.out.println("Job title: Information Security Analyst");
+        System.out.println("Why: 29% projected job growth through 2034, much faster than average, and strong demand for cybersecurity skills");
+        System.out.println("Median salary: $124,910");
+    }
+}
